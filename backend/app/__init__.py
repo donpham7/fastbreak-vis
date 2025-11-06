9686fa0513ffefbe37826cbda8e8bd8ebb99d581
WayneGretzky1 <brockbye@outlook.com>
2025-11-06 00:48:41 -0600

    fix init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,7 +8,8 @@ load_dotenv()
 cache = Cache()
 
 def create_app():
-    app = Flask(__name__, static_folder="../frontend/build", static_url_path="")
+    static_dir = os.path.join(os.path.dirname(__file__), "..", "frontend", "build")
+    app = Flask(__name__, static_folder=static_dir, static_url_path="")
     app.config.from_mapping({
         "CACHE_TYPE": "simple",
         "CACHE_DEFAULT_TIMEOUT": 300,
@@ -18,4 +19,13 @@ def create_app():
     from backend.app.routes import main
     app.register_blueprint(main)
 
+        # Serve React build
+    @app.route("/", defaults={"path": ""})
+    @app.route("/<path:path>")
+    def serve(path):
+        if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+            return app.send_static_file(path)
+        else:
+            return app.send_static_file("index.html")
+        
     return app
